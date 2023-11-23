653bde745f699b948b43b3649c8daa752137f459
rlal2022 <lrahul2010@gmail.com>
2023-11-23 05:03:36 -0800

    landing page content added

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,15 +32,38 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
-      <div className="view-more-data-btn-container">
-        <Button
-          type="default"
-          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
-          onClick={() => history.push('/graphs')}
-        >
-          View the Data
-        </Button>
+      <div className="graphs-section">
+        <div className="grant-rates-container">
+          <img src={GrantRatesByOfficeImg} alt="" />
+          <p>Search Grant Rates By Office</p>
+        </div>
+        <div className="grant-rates-by-nationality-container">
+          <img src={GrantRatesByNationalityImg} alt="" />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className="grant-rates-by-time-container">
+          <img src={GrantRatesOverTimeImg} alt="" />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+
+        <div className="view-more-data-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/graphs')}
+          >
+            View the Data
+          </Button>
+        </div>
+        <div className="download-data-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/download')}
+          >
+            Download the Data
+          </Button>
+        </div>
       </div>
 
       <div className="middle-section">
@@ -61,7 +84,35 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <h2>36%</h2>
+          <h3>
+            By the end of the Trump administration, the average asylum office
+            grant rate had fallen 36 percent from an average of 44 percent in
+            fiscal year 2016 to 28 percent in fiscal year 2020.
+          </h3>
+          <h2>5%</h2>
+          <h3>
+            The New York asylum office grant rate dropped to 5 percent in fiscal
+            year 2020.
+          </h3>
+          <h2>6x Lower</h2>
+          <h3>
+            Between fiscal year 2017 and 2020, the New York asylum office's
+            average grant rate was six times lower than the San Francisco asylum
+            office.
+          </h3>
+        </div>
+        <div className="read-more-btn-container">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/download')}
+          >
+            Read More
+          </Button>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
